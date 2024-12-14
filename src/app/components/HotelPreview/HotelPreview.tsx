5514798a04a533094c46dc1332eeb1ba9c651a82
Martin Mikayelyan <martinarmenich@gmail.com>
2024-12-14 21:09:09 +0400

    better styling

--- src/app/components/HotelPreview/HotelPreview.tsx
+++ src/app/components/HotelPreview/HotelPreview.tsx
@@ -1,59 +1,33 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
-import { motion } from "framer-motion";
 import RoomPreview from "./RoomPreview/RoomPreview";
+import Button from "../Button/Button";
 
 export default function HotelPreview() {
   return (
     <section>
-      <div className="px-4 pb-20 grid grid-cols-[1404px] grid-rows-3 gap-y-8">
-        <div className="flex flex-col col-start-1 row-start-1 col-end-1 row-end-1 sticky top-[110px] w-full">
-          <div className="grid flex-grow grid-cols-[1368px] grid-rows-[431.984px] px-[18px] py-9">
-            <div className="flex flex-col row-start-1 col-start-1 row-end-2 col-end-2 self-center justify-self-center w-[769.5px]">
-              <div className="self-center mb-[108px] order-1 w-full">
-                <h2 className="text-[25.875px] text-center font-normal">
+      <div className="px-4 pb-20 grid grid-cols-[1404px] grid-rows-3 gap-y-10">
+        <div className="sticky top-[110px]">
+          <div className="px-4 py-9">
+            <div className="flex flex-col self-center justify-self-center w-[770px]">
+              <div className="mb-20">
+                <h2 className="text-2xl text-center font-normal">
                   Rooms & Suites
                 </h2>
               </div>
-              <div className="self-center mb-[27.0023px] order-2 text-start w-full">
-                <p className="text-[47.25px] font-normal text-center leading-[51.975px]">
+              <div className="mb-7">
+                <p className="text-5xl font-normal text-center">
                   Fresh linens, curated wall art and hand-picked minibars keep
                   The Tsakhkadé rooms simple, smart and sophisticated.
                 </p>
               </div>
-              <div className="self-center h-[40px] order-3 w-[200px] border border-solid border-grayCustom">
-                <Link
-                  href="#"
-                  className="text-black cursor-pointer"
-                  aria-label="Explore all rooms"
-                >
-                  <span className="items-center flex justify-center h-[38px] w-[198px]">
-                    <span className="italic font-normal leading-[22.4px]">
-                      Explore All Rooms
-                    </span>
-                    <span className="ml-1">
-                      <motion.svg
-                        style={{ fill: "currentcolor" }}
-                        initial={{ rotate: 0 }}
-                        whileHover={{ rotate: 30 }}
-                        transition={{ duration: 0.3, ease: "easeInOut" }}
-                        data-bbox="30.718 20 138.564 160"
-                        viewBox="0 0 200 200"
-                        height="16"
-                        width="16"
-                        xmlns="http://www.w3.org/2000/svg"
-                        data-type="shape"
-                      >
-                        <g>
-                          <path d="M30.718 20l138.564 80-138.564 80 53.417-80.027L30.718 20z"></path>
-                        </g>
-                      </motion.svg>
-                    </span>
-                  </span>
-                </Link>
-              </div>
+              <Button
+                whileHover={{ backgroundColor: "#000000", color: "#ffffff" }}
+                transition={{ duration: 0.3 }}
+                title="Explore All Rooms"
+                containerStyles="self-center items-center justify-center w-52 h-10 border border-solid border-grayCustom"
+              />
             </div>
           </div>
         </div>
--- src/app/components/HotelPreview/RoomPreview/RoomPreview.tsx
+++ src/app/components/HotelPreview/RoomPreview/RoomPreview.tsx
@@ -2,77 +2,62 @@
 
 import React from "react";
 import Image from "next/image";
-// import Link from "next/link";
 import Button from "../../Button/Button";
 
 function RoomPreview() {
   return (
-    <div className="flex flex-col col-start-1 row-start-2 col-end-2 row-end-3 h-[504.656px] sticky top-[110px] w-[1404px] bg-cardBg">
-      <div className="p-[18px] grid flex-grow grid-cols-[1368px] grid-rows-[468.656px] h-full w-full">
-        <div className="self-end flex col-start-1 row-start-1 col-end-1 row-end-1 h-[468.5px] justify-self-start w-[1368.01px]">
-          <div className="self-start grid grid-cols-[820.797px] grid-rows-[468px] h-[468px] order-1 w-[820.797px]">
-            <Image
-              src="https://static.wixstatic.com/media/c837a6_96fae0e0e61c4da7b15488f2ab2c7538~mv2.jpg/v1/fill/w_1642,h_936,fp_0.86_0.49,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/The_Executive_01.jpg"
-              width={821}
-              height={468}
-              alt="Executive Room with King-size Bed"
-            />
-          </div>
-          <div className="flex flex-col h-[293.648px] order-2 w-[547.195px]">
-            <div className="pl-[18px] flex flex-col h-full w-full">
-              <div className="mb-9 w-[529.195px]">
-                <h3 className="leading-[34.65px] w-full">
-                  <span className="text-[31.5px] font-normal text-left italic">
-                    The Executive
-                  </span>
-                </h3>
+    <section className="sticky top-[110px] bg-cardBg">
+      <div className="p-4">
+        <div className="flex">
+          <Image
+            src="https://static.wixstatic.com/media/c837a6_96fae0e0e61c4da7b15488f2ab2c7538~mv2.jpg/v1/fill/w_1642,h_936,fp_0.86_0.49,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/The_Executive_01.jpg"
+            width={821}
+            height={468}
+            alt="Executive Room with King-size Bed"
+          />
+          <div className="flex-grow">
+            <div className="pl-4">
+              <div className="mb-9">
+                <h3 className="text-3xl font-normal italic">The Executive</h3>
               </div>
-              <div className="grid grid-cols-[529.195px] grid-rows-[163px] mb-9">
-                <div className="flex flex-col gap-[1px] w-full h-full">
-                  <div className="grid grid-cols-[529.195px] grid-rows-[40px] h-[40px] w-full border-b border-descGapBg">
-                    <div className="self-center col-start-1 row-start-1 col-end-2 row-end-2">
-                      <p>
-                        <span className="font-normal text-base">
-                          2 Adults (30m2)
-                        </span>
-                      </p>
+              <div className="mb-9">
+                <div className="flex flex-col gap-[1px]">
+                  <div className="grid h-[40px] w-full border-b border-descGapBg">
+                    <div className="self-center">
+                      <span className="font-normal text-base">
+                        2 Adults (30m2)
+                      </span>
                     </div>
                   </div>
-                  <div className="grid grid-cols-[529.195px] grid-rows-[40px] h-[40px] w-full border-b border-descGapBg">
-                    <div className="self-center col-start-1 row-start-1 col-end-2 row-end-2">
-                      <p>
-                        <span className="font-normal text-base">
-                          King size bed (76 x 80 inches)
-                        </span>
-                      </p>
+                  <div className="grid h-[40px] w-full border-b border-descGapBg">
+                    <div className="self-center">
+                      <span className="font-normal text-base">
+                        King size bed (76 x 80 inches)
+                      </span>
                     </div>
                   </div>
-                  <div className="grid grid-cols-[529.195px] grid-rows-[40px] h-[40px] w-full border-b border-descGapBg">
-                    <div className="self-center col-start-1 row-start-1 col-end-2 row-end-2">
-                      <p>
-                        <span className="font-normal text-base">
-                          Separate seating area
-                        </span>
-                      </p>
+                  <div className="grid h-[40px] w-full border-b border-descGapBg">
+                    <div className="self-center">
+                      <span className="font-normal text-base">
+                        Separate seating area
+                      </span>
                     </div>
                   </div>
-                  <div className="grid grid-cols-[529.195px] grid-rows-[40px] h-[40px] w-full border-b border-descGapBg">
-                    <div className="self-center col-start-1 row-start-1 col-end-2 row-end-2">
-                      <p>
-                        <span className="font-normal text-base">
-                          Spacious work desk
-                        </span>
-                      </p>
+                  <div className="grid h-[40px] w-full border-b border-descGapBg">
+                    <div className="self-center">
+                      <span className="font-normal text-base">
+                        Spacious work desk
+                      </span>
                     </div>
                   </div>
                 </div>
               </div>
-              <Button title="Book Now" textStyles="italic" />
+              <Button title="Book Now" textStyles="hover:underline" />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
